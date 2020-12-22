30e2854cc2da2dd71593d0992057a07a5d4f1dd4
Kai Peng Li <oopanpan@gmail.com>
2020-12-22 22:38:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,7 +19,7 @@ def nyc_pigeon_organizer(data)
   final_array.each do |final_key, name |
     data.each do |input_key, input_value|
       if !name[input_key]
-        name[input_key] = {}
+        name[input_key] = []
         binding.pry
       end
     end
