e085774cc282b18f24767f9f062c8f8b14524ee6
Kai Peng Li <oopanpan@gmail.com>
2020-12-22 22:21:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #build the first level hash with name extracted
+  #leave the origianl symbol and turn second level key into String
+  #assign that as value to the key
+  final_array = {}
+  data.each do |k1,v1|
+    v1.each do |k2,v2|
+      v2.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+      end
+    end
+  end
+  
+  data.each do |k1,v1|
+    final_array
+  end
 end
